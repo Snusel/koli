f824e15928f91c4c16a75301150bdd2f5a3326de
Snusel <snusel@unitybox.de>
2015-02-07 23:45:01 +0100

    das Kopirt ein 2 rutorrent
    das Skript Kopirt alle daten für ein 2 oder 3 oder 4 oder 5 rutorrent
    Und Startet die Config datein zur Bearbeitung
    Und man Legt den Jeweiligen User An

--- skripte/rtorrent.sh
+++ skripte/rtorrent.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#/bin/sh
 #
 
 # script by Snusel
@@ -6,10 +6,20 @@
 ######################################################
 # Bitte Anpassen
 
+pfad=home
 grupe=users
 
 ######################################################
 
+echo "-------------------------------------"
+echo "User Anlägen"
+echo "-------------------------------------"
+echo ""
+echo -n "User: "
+read user
+useradd $user -g $grupe -m -d /$pfad/$user -s /bin/sh
+chown $user.$grupe /$pfad/$user -R
+passwd $user
 echo "-------------------------------------"
 echo "Von welchem User soll kopiert werden?"
 echo "-------------------------------------"
@@ -23,16 +33,18 @@ echo ""
 echo -n "User: "
 read user2
 
-cp -R /home/$user1/.rtorrent-session/ /home/$user2/
-chgrp $grupe /home/$user2/.rtorrent-session/
-chmod 0775 /home/$user2/.rtorrent-session/
-chown $user2 /home/$user2/.rtorrent-session/
+chmod 0775 /$pfad/$user2/
+
+cp -R /$pfad/$user1/.rtorrent-session/ /home/$user2/
+chgrp $grupe /$pfad/$user2/.rtorrent-session/
+chmod 0775 /$pfad/$user2/.rtorrent-session/
+chown $user2 /$pfad/$user2/.rtorrent-session/
 #
 cp /home/$user1/.rtorrent.rc /home/$user2/
-chgrp $grupe /home/$user2/.rtorrent.rc
-chown $user2 /home/$user2/.rtorrent.rc
+chgrp $grupe /$pfad/$user2/.rtorrent.rc
+chown $user2 /$pfad/$user2/.rtorrent.rc
 #
-cp -R /home/$user1/log/ /home/$user2/
+cp -R /$pfad/$user1/log/ /home/$user2/
 #
 cp -R /var/www/$user1/ /var/www/$user2/
 chgrp -R www-data /var/www/$user2/
@@ -43,4 +55,10 @@ cp /etc/apache2/sites-available/$user1 /etc/apache2/sites-available/$user2
 ln -s /etc/apache2/sites-available/$user2 /etc/apache2/sites-enabled/$user2
 #
 cp /etc/init.d/$user1-init /etc/init.d/$user2-init
-#update-rc.d $user2-init defaults
+
+nano /$pfad/$user2/.rtorrent.rc
+nano /etc/apache2/sites-available/$user2
+nano /etc/init.d/$user2-init
+nano /var/www/$user2/conf/config.php
+update-rc.d $user2-init defaults
+service $user2-init start
